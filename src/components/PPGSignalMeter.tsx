1ac43628912b3def9a865c55f8ee6dee5567aa61
charly7978 <Carlos_ameghino@hotmail.com>
2025-02-28 07:10:15 -0300

    Update PPGSignalMeter.tsx

--- src/components/PPGSignalMeter.tsx
+++ src/components/PPGSignalMeter.tsx
@@ -43,7 +43,7 @@ const PPGSignalMeter = ({
   const verticalScale = 30.0;
   const SMOOTHING_FACTOR = 0.55;
   const TARGET_FPS = 150;
-  const FRAME_TIME = 400 / TARGET_FPS;
+  const FRAME_TIME = 1000 / TARGET_FPS;
   const BUFFER_SIZE = 40;
 
   useEffect(() => {
