eae7061862fd75e1b6dafb208de460df9bbe9d1f
charly7978 <Carlos_ameghino@hotmail.com>
2025-03-02 03:53:06 -0300

    Update BloodPressureCalculator.ts

--- src/modules/BloodPressureCalculator.ts
+++ src/modules/BloodPressureCalculator.ts
@@ -4,8 +4,8 @@ export class BloodPressureCalculator {
   // Constants for blood pressure calculation
   private readonly BP_BASELINE_SYSTOLIC = 1; // Base systolic value
   private readonly BP_BASELINE_DIASTOLIC = 1; // Base diastolic value
-  private readonly BP_PTT_COEFFICIENT = 0.95; // Increased for more significant variations
-  private readonly BP_AMPLITUDE_COEFFICIENT = 1.20; // Increased for more sensitivity 0.15
+  private readonly BP_PTT_COEFFICIENT = 1.95; // Increased for more significant variations
+  private readonly BP_AMPLITUDE_COEFFICIENT = 1.50; // Increased for more sensitivity 0.15
   private readonly BP_STIFFNESS_FACTOR = 1.28; // Increased from 0.06 for more variation  0.08
   private readonly BP_SMOOTHING_ALPHA = 1.58; // Reduced for more natural fluctuations 0.18
   private readonly BP_QUALITY_THRESHOLD = 0.050; // 0.50
