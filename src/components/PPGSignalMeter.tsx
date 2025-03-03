6adc25d2a2970ec58126aeadde47abdafed2e5ec
charly7978 <Carlos_ameghino@hotmail.com>
2025-03-03 15:42:52 -0300

    Update PPGSignalMeter.tsx

--- src/components/PPGSignalMeter.tsx
+++ src/components/PPGSignalMeter.tsx
@@ -34,7 +34,7 @@ const PPGSignalMeter = ({
   const lastArrhythmiaTime = useRef<number>(0);
   const arrhythmiaCountRef = useRef<number>(0);
   
-  const WINDOW_WIDTH_MS = 2500;
+  const WINDOW_WIDTH_MS = 7500;
   const CANVAS_WIDTH = 450;
   const CANVAS_HEIGHT = 400;
   const GRID_SIZE_X = 555;
