cf664b45eb7dd9d9bd27cb5ae80da5b21bfac558
charly7978 <Carlos_ameghino@hotmail.com>
2025-03-04 07:53:38 -0300

    f

--- src/utils/CircularBuffer.ts
+++ src/utils/CircularBuffer.ts
@@ -1,33 +1,46 @@
-
-interface PPGDataPoint {
+export interface PPGDataPoint {
   time: number;
   value: number;
   isArrhythmia: boolean;
+  isWaveStart: boolean;
 }
 
 export class CircularBuffer {
   private buffer: PPGDataPoint[];
-  private maxSize: number;
+  private size: number;
+  private currentIndex: number;
+  private isFull: boolean;
 
   constructor(size: number) {
-    this.buffer = [];
-    this.maxSize = size;
+    this.buffer = new Array(size);
+    this.size = size;
+    this.currentIndex = 0;
+    this.isFull = false;
   }
 
-  push(point: PPGDataPoint): void {
-    this.buffer.push(point);
-    if (this.buffer.length > this.maxSize) {
-      this.buffer.shift();
+  push(point: PPGDataPoint) {
+    this.buffer[this.currentIndex] = point;
+    this.currentIndex = (this.currentIndex + 1) % this.size;
+    if (this.currentIndex === 0) {
+      this.isFull = true;
     }
   }
 
   getPoints(): PPGDataPoint[] {
-    return [...this.buffer];
+    if (!this.isFull) {
+      return this.buffer.slice(0, this.currentIndex);
+    }
+    
+    const points = new Array(this.size);
+    for (let i = 0; i < this.size; i++) {
+      points[i] = this.buffer[(this.currentIndex + i) % this.size];
+    }
+    return points.filter(point => point !== undefined);
   }
 
-  clear(): void {
-    this.buffer = [];
+  clear() {
+    this.buffer = new Array(this.size);
+    this.currentIndex = 0;
+    this.isFull = false;
   }
 }
-
-export type { PPGDataPoint };
